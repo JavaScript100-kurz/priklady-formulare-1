4c2fbc490c95255636b8721e315b37dd71aa1ebf
Luděk Roleček <ludek@rolecek.cz>
2023-07-25 19:26:34 +0200

    Aktualizace

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ Události:
 
 event.target
 */
+
+const formular = document.querySelector('#formular');
+const jmenoEl = document.querySelector('#jmeno');
+const prijmeniEl = document.querySelector('#prijmeni');
+const souhlasEl = document.querySelector('#souhlas');
+const mestoEl = document.querySelector('#mesto');
+
+const zpravaEl = document.querySelector('#zprava');
+
+
+formular.addEventListener('submit', (event) => {
+	// zabranime standardnimu odeslani formulare
+	event.preventDefault();
+
+	zpravaEl.textContent = `Tvoje jméno je ${jmenoEl.value} ${prijmeniEl.value} a tvoje směrovací číslo je ${mestoEl.value}`;
+
+	console.log( mestoEl.value );
+
+	console.log( souhlasEl.checked );
+
+
+	// jmenoEl.value = '';
+	// prijmeniEl.value = '';
+
+	console.log('formular odeslan');
+});
+
+
+prijmeniEl.addEventListener('input', () => {
+	console.log('input', prijmeniEl.value);
+});
+
+prijmeniEl.addEventListener('change', () => {
+	console.log('change', prijmeniEl.value);
+});
+
+
+
+souhlasEl.addEventListener('change', () => {
+	console.log(souhlasEl.checked);
+});
+
+mestoEl.addEventListener('change', () => {
+	console.log(mestoEl.value);
+});
